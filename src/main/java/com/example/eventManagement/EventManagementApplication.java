d9753b6004c34114726c1e46795cc47abda94e41
Aryan-Dalal <dalalaryan957@gmail.com>
2026-03-10 19:29:17 +0530

    Jenkins file added for server Deployment

--- src/main/java/com/example/eventManagement/EventManagementApplication.java
+++ src/main/java/com/example/eventManagement/EventManagementApplication.java
@@ -2,14 +2,18 @@ package com.example.eventManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EventManagementApplication {
+public class EventManagementApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EventManagementApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventManagementApplication.class, args);
-		System.out.println("Hello and Welcome To Our Event Management Project!!");
 	}
-
 }
-
